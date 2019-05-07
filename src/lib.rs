c3f8e78b3f2435c0d156553925725302d5131023
dbr <dbr.onix@gmail.com>
2019-05-07 21:44:50 +0930

    Implement SeededRand and docstrings

--- src/lib.rs
+++ src/lib.rs
@@ -2,50 +2,63 @@ extern crate rand;
 
 use rand::Rng;
 
-/// Generate a random numnber between 0.0 and 1.0
+/// Returns a random numnber between 0.0 and 1.0
 pub fn random() -> f64 {
     rand::random::<f64>()
 }
 
-/// Generates a random number in the specified range
+/// Returns a random number in the specified range
 pub fn randrange(min: f64, max: f64) -> f64 {
     rand::thread_rng().gen_range(min, max)
 }
 
+/// Returns a random integer in the specified range
 pub fn randint(min: i64, max: i64) -> i64 {
     rand::thread_rng().gen_range(min, max)
 }
 
 use rand::seq::SliceRandom;
 
+/// In-place shuffle of the given object
 pub fn shuffle<T>(obj: &mut Vec<T>) {
     let mut rng = rand::thread_rng();
     obj.shuffle(&mut rng);
 }
 
+/// Return a reference to a randomly selected item in the given object
 pub fn choose<T>(obj: &Vec<T>) -> Option<&T> {
     let mut rng = rand::thread_rng();
     obj.choose(&mut rng)
 }
 
 pub fn with_seed(seed: u64) -> SeededRand {
-    SeededRand { seed: seed }
+    use rand::SeedableRng;
+
+    SeededRand {
+        rng: rand::rngs::StdRng::seed_from_u64(seed),
+    }
 }
 
 pub struct SeededRand {
-    seed: u64,
+    rng: rand::rngs::StdRng,
 }
 
 impl SeededRand {
-    pub fn random() -> f64 {
-        0.0
+    pub fn random(&mut self) -> f64 {
+        self.rng.gen()
     }
 
-    pub fn randrange(min: f64, max: f64) -> f64 {
-        2.0
+    pub fn randrange(&mut self, min: f64, max: f64) -> f64 {
+        self.rng.gen_range(min, max)
     }
 
-    pub fn shuffle() {}
+    pub fn shuffle<T>(obj: &mut Vec<T>) {
+        let mut rng = rand::thread_rng();
+        obj.shuffle(&mut rng);
+    }
 
-    pub fn choice() {}
+    pub fn choose<T>(obj: &Vec<T>) -> Option<&T> {
+        let mut rng = rand::thread_rng();
+        obj.choose(&mut rng)
+    }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,4 +1,4 @@
-use easyrand::{choose, random, randrange};
+use easyrand::{choose, random, randrange, with_seed};
 
 #[test]
 fn test_readme() {
@@ -37,4 +37,29 @@ fn test_choose() {
     let things = vec!["abc", "def"];
     let r = choose(&things).unwrap();
     assert!(*r == "abc" || *r == "def");
+}
+
+#[test]
+fn test_seeded() {
+    let mut s = with_seed(42);
+    let r1 = dbg!(s.random());
+    let r2 = dbg!(s.random());
+    let r3 = dbg!(s.random());
+
+    // Check values are consistent
+    assert!(r1.abs() - 0.9728880888443334 < 1e-8);
+    assert!(r2.abs() - 0.5240921244397169 < 1e-8);
+    assert!(r3.abs() - 0.12255363798249008 < 1e-8);
+
+    // Check same results with new instance
+    let mut s2 = with_seed(42);
+    assert!(s2.random() == r1);
+    assert!(s2.random() == r2);
+    assert!(s2.random() == r3);
+
+    // Check different seed gives different result
+    let mut s3 = with_seed(43);
+    assert!(s3.random() != r1);
+    assert!(s3.random() != r2);
+    assert!(s3.random() != r3);
 }
